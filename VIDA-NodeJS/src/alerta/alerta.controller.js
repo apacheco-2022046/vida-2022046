403e54fed9547fb2d24e02db2ce189b489212deb
apacheco-2022046 <apacheco-2022046@kinal.edu.gt>
2024-06-10 02:17:55 -0600

    Guardar imagen en mongo en base64 en sucio

--- VIDA-NodeJS/src/alerta/alerta.controller.js
+++ VIDA-NodeJS/src/alerta/alerta.controller.js
@@ -12,15 +12,15 @@ const __dirname = path.dirname(__filename)
 const uploadDir = path.join(__dirname, '../public/uploads')
 
 
-
-export const save = async (req, res) => {
+//Este responde con error 400
+export const save8 = async (req, res) => {
     try {
         let data = req.body;
         const validacion = validar(req.file, 'Y')
         if (validacion == '') {
             const imagen = new Imagen({
                 nombresDesaparecido: data.nombresDesaparecido,
-                apellidosDesaparecido: data.apellidosDesaparecido,
+                /*apellidosDesaparecido: data.apellidosDesaparecido,
                 edadDesaparecido : data.edadDesaparecido,
                 estaturaDesaparecido : data.estaturaDesaparecido,
                 descripcionDesaparecido : data.descripcionDesaparecido,
@@ -38,22 +38,87 @@ export const save = async (req, res) => {
                 edadDenunciante : data.edadDenunciante,
                 direccionViviendaDenunciante : data.direccionViviendaDenunciante,
                 sexoDenunciante : data.sexoDenunciante,
-                estadoAlerta : data.estadoAlerta
-
-
-
+                estadoAlerta : data.estadoAlerta/*/
 
             })
             await imagen.save()
-            return res.send({message: 'Alert saved successfully'})
+            return res.send({message: 'Alert saved successfully', imagen})
         }
         return res.status(400).send({message: 'Error saving alert'})
     } catch (error) {
         console.error(error);
-        return res.status(500).send({ message: 'Error server' });
+        return res.status(500).send({ message: 'Error server', validacion });
     }
 
+}
+    
 
+//Recibe todos los campos menos la imagen
+export const save = async (req, res) => {
+    //console.log(req.body)
+   // return res.send({ message: req.body });
+    
+    
+        try {
+            let data = req.body;
+           if (data) {
+                data.fotoDesaparecido = req.body.fotoDesaparecido.uri
+            }
+            console.log(data)
+        let alerta = new Alerta(data)
+        await alerta.save();
+        return res.send({ message: 'Alert saved successfully' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({ message: 'Error saving alert' });
+    }
+}
+
+export const save5 = async (req, res) => {
+    try {
+        console.log('Starting save process...')
+        console.log('Request headers:', req.headers);
+        let data = req.body;
+        console.log('Received data:', data);
+        const validacion = validar(req.file, 'Y')
+        console.log('Validation result:', validacion);
+        if (validacion == '') {
+            if (req.file) {
+                data.fotoDesaparecido = req.file.filename;
+            }
+            const imagen = new Imagen({
+                nombresDesaparecido: data.nombresDesaparecido,
+                apellidosDesaparecido: data.apellidosDesaparecido,
+                edadDesaparecido : data.edadDesaparecido,
+                estaturaDesaparecido : data.estaturaDesaparecido,
+                descripcionDesaparecido : data.descripcionDesaparecido,
+                direccionVivienda : data.direccionVivienda,
+                direccionDesaparicion : data.direccionDesaparicion,
+                fechaDesaparicion : data.fechaDesaparicion,
+                sexoDesaparecido : data.sexoDesaparecido,
+                fotoDesaparecido : data.fotoDesaparecido,
+                nombresDenunciante: data.nombresDenunciante,
+                apellidosDenunciante : data.apellidosDenunciante,
+                DPIDenunciente : data.DPIDenunciente,
+                telefonoDenunciante : data.telefonoDenunciante,
+                emailDenunciante : data.emailDenunciante,
+                parentescoDenunciante : data.parentescoDenunciante,
+                edadDenunciante : data.edadDenunciante,
+                direccionViviendaDenunciante : data.direccionViviendaDenunciante,
+                sexoDenunciante : data.sexoDenunciante,
+                estadoAlerta : data.estadoAlerta
+            })
+            console.log('Created new Imagen:', imagen);
+            await imagen.save()
+            console.log('Saved Imagen successfully');
+            return res.send({message: 'Alert saved successfully', imagen})
+        }
+        console.log('Error in validation');
+        return res.status(400).send({message: 'Error saving alert'})
+    } catch (error) {
+        console.error('Error in save process:', error);
+        return res.status(500).send({ message: 'Error server', validacion });
+    }
 }
 
 
